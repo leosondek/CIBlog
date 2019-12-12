921baa9c41609e2b6f69a4682bc98b69af5263f6
Leo Sondek <leosondek@Leos-MacBook-Pro.local>
2019-12-12 15:29:37 +0800

    truncated post index

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -2,7 +2,9 @@
 <?php foreach($posts as $post) : ?>
     <h3><?= $post['title']; ?></h3>
     <small class="post-date">Posted On: <?= $post['created_at']; ?> </small> <br>
-    <?= $post['body']; ?>
+
+//word_limiter -> helper(text)
+    <?= word_limiter ($post['body'], 150); ?>
     <br>
     <br>
     <p> <a class= "btn btn-default" href="<?= site_url('/posts/'.$post['slug']); ?>">Read More</a> </p>
